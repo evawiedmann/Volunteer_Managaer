ced03faf732cea13e2e7c3c0810171ef52d6ae18
Eva Wiedmann <evawiedmann@gmail.com>
2020-01-10 13:15:42 -0800

    continue work on error message for testing

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,123 +1,123 @@
-# class Album
-#
-#   attr_accessor :name, :year, :id, :genre, :artist, :status, :cost
-#
-#   def initialize(attributes)
-#     @name = attributes.fetch(:name)
-#     @id = attributes.fetch(:id).to_i
-#     @year = attributes[:year].to_i
-#     @genre = attributes.fetch(:genre)
-#     @artist = attributes.fetch(:artist)
-#     @status = attributes.fetch(:status)
-#     @cost = attributes.fetch(:cost).to_f
-#   end
+class Project
+
+  attr_accessor :name, :year, :id, :genre, :volunteer, :status, :cost
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id).to_i
+    @year = attributes[:year].to_i
+    @genre = attributes.fetch(:genre)
+    @volunteer = attributes.fetch(:volunteer)
+    @status = attributes.fetch(:status)
+    @cost = attributes.fetch(:cost).to_f
+  end
 #
 #   def self.all()
-#     returned_albums = DB.exec("SELECT * FROM albums;")
-#     albums = []
-#     returned_albums.each() do |album|
-#       name = album.fetch("name")
-#       id = album.fetch("id").to_i
-#       year = album.fetch("year").to_i
-#       genre = album.fetch("genre")
-#       artist = album.fetch("artist")
-#       status = album.fetch("status")
-#       cost = album.fetch("cost").to_f
-#       albums.push(Album.new({:name => name, :id => id, :year => year, :genre => genre, :artist => artist, :status => status, :cost => cost}))
+#     returned_projects = DB.exec("SELECT * FROM projects;")
+#     projects = []
+#     returned_projects.each() do |project|
+#       name = project.fetch("name")
+#       id = project.fetch("id").to_i
+#       year = project.fetch("year").to_i
+#       genre = project.fetch("genre")
+#       volunteer = project.fetch("volunteer")
+#       status = project.fetch("status")
+#       cost = project.fetch("cost").to_f
+#       projects.push(Project.new({:name => name, :id => id, :year => year, :genre => genre, :volunteer => volunteer, :status => status, :cost => cost}))
 #     end
-#     albums
+#     projects
 #   end
 #
 #   def self.alphabetize
-#     albums = self.all
-#     albums.sort { |a, b| a.name.downcase <=> b.name.downcase }
+#     projects = self.all
+#     projects.sort { |a, b| a.name.downcase <=> b.name.downcase }
 #   end
 #
 #   def self.sort_year
-#     albums = self.all
-#     albums.sort { |a, b| b.year <=> a.year }
+#     projects = self.all
+#     projects.sort { |a, b| b.year <=> a.year }
 #   end
 #
 #   def self.sort_cost
-#     albums = self.all
-#     albums.sort { |a, b| a.cost <=> b.cost }
+#     projects = self.all
+#     projects.sort { |a, b| a.cost <=> b.cost }
 #   end
 #
 #   def self.random
-#     returned_albums = DB.exec("SELECT * FROM albums ORDER BY RANDOM() LIMIT 1;")
-#     albums = []
-#     returned_albums.each() do |album|
-#       name = album.fetch("name")
-#       id = album.fetch("id").to_i
-#       year = album.fetch("year").to_i
-#       genre = album.fetch("genre")
-#       artist = album.fetch("artist")
-#       status = album.fetch("status")
-#       cost = album.fetch("cost").to_f
-#       albums.push(Album.new({:name => name, :id => id, :year => year, :genre => genre, :artist => artist, :status => status, :cost => cost}))
+#     returned_projects = DB.exec("SELECT * FROM projects ORDER BY RANDOM() LIMIT 1;")
+#     projects = []
+#     returned_projects.each() do |project|
+#       name = project.fetch("name")
+#       id = project.fetch("id").to_i
+#       year = project.fetch("year").to_i
+#       genre = project.fetch("genre")
+#       volunteer = project.fetch("volunteer")
+#       status = project.fetch("status")
+#       cost = project.fetch("cost").to_f
+#       projects.push(Project.new({:name => name, :id => id, :year => year, :genre => genre, :volunteer => volunteer, :status => status, :cost => cost}))
 #     end
-#     albums
+#     projects
 #   end
 #
 #   def save
-#     result = DB.exec("INSERT INTO albums (name, year, genre, artist, status) VALUES ('#{@name}', '#{@year}', '#{@genre}', '#{@artist}', '#{@status}') RETURNING id;")
+#     result = DB.exec("INSERT INTO projects (name, year, genre, volunteer, status) VALUES ('#{@name}', '#{@year}', '#{@genre}', '#{@volunteer}', '#{@status}') RETURNING id;")
 #     @id = result.first().fetch("id").to_i
 #   end
 #
-#   def ==(album_to_compare)
-#     if album_to_compare != nil
-#       self.name() == album_to_compare.name()
+#   def ==(project_to_compare)
+#     if project_to_compare != nil
+#       self.name() == project_to_compare.name()
 #     else
 #       false
 #     end
 #   end
 #
 #   def self.clear
-#     DB.exec("DELETE FROM albums *;")
+#     DB.exec("DELETE FROM projects *;")
 #   end
 #
 #   def self.find(id)
-#     album = DB.exec("SELECT * FROM albums WHERE id = #{id};").first
-#     if album
-#       name = album.fetch("name")
-#       id = album.fetch("id").to_i
-#       year = album.fetch("year").to_i
-#       genre = album.fetch("genre")
-#       artist = album.fetch("artist")
-#       status = album.fetch("status")
-#       cost = album.fetch("cost").to_f
-#       Album.new({:name => name, :id => id, :year => year, :genre => genre, :artist => artist, :status => status, :cost => cost})
+#     project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+#     if project
+#       name = project.fetch("name")
+#       id = project.fetch("id").to_i
+#       year = project.fetch("year").to_i
+#       genre = project.fetch("genre")
+#       volunteer = project.fetch("volunteer")
+#       status = project.fetch("status")
+#       cost = project.fetch("cost").to_f
+#       Project.new({:name => name, :id => id, :year => year, :genre => genre, :volunteer => volunteer, :status => status, :cost => cost})
 #     else
 #       nil
 #     end
 #   end
 #
 #   def self.search_name(name)
-#     albums = self.all
-#     albums.select { |album| /#{name}/i.match? album.name }
+#     projects = self.all
+#     projects.select { |project| /#{name}/i.match? project.name }
 #   end
 #
-#   def update(name, year, genre, artist, cost)
+#   def update(name, year, genre, volunteer, cost)
 #     @name = name
 #     @year = year
 #     @genre = genre
-#     @artist = artist
+#     @volunteer = volunteer
 #     @cost = cost
-#     DB.exec("UPDATE albums SET name = '#{@name}', year = '#{@year}', genre = '#{@genre}', artist = '#{@artist}', cost = '#{@cost}' WHERE id = #{id};")
+#     DB.exec("UPDATE projects SET name = '#{@name}', year = '#{@year}', genre = '#{@genre}', volunteer = '#{@volunteer}', cost = '#{@cost}' WHERE id = #{id};")
 #   end
 #
 #   def delete()
-#     DB.exec("DELETE FROM albums WHERE id = #{@id};")
-#     DB.exec("DELETE FROM songs WHERE album_id = #{@id};")
+#     DB.exec("DELETE FROM projects WHERE id = #{@id};")
+#     DB.exec("DELETE FROM songs WHERE project_id = #{@id};")
 #   end
 #
 #   def sold()
 #     @status = false
-#     DB.exec("UPDATE albums SET status = '#{@status}' WHERE id = #{id};")
+#     DB.exec("UPDATE projects SET status = '#{@status}' WHERE id = #{id};")
 #   end
 #
 #   def songs
-#     Song.find_by_album(self.id)
+#     Song.find_by_project(self.id)
 #   end
 #
-# end
+end
